0cfd8fe06165e86479c9d09b61347d25de2fb6c0
Alexander Chu <alex.p.chu@gmail.com>
2014-10-17 17:55:41 -0700

    Make server skeleton

--- server.py
+++ server.py
@@ -3,29 +3,11 @@ import os
 from flask import Flask, render_template, send_file
 from HogOnline import HogOnline
 
-hog = HogOnline()
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return render_template("play.html")
-  
-@app.route('/js/<path>')
-def static_proxy(path):
-    # send_static_file will guess the correct MIME type
-    return send_file(os.path.join('js', path))
-
-@app.route('/join/<name>')
-def join(name):
-    return hog.GetEmptyGame(name)
-
-@app.route('/play/<game>/<player_id>/<dice>')
-def make_move(game,player_id,dice):
-    return hog.RollDice(game, player_id, dice)
-
-@app.route('/status/<game>')
-def game_status(game):
-    return hog.GameStatus(game)
-
+#@app.route('/YOUR BEAUTIFUL URL HERE')
+#def HANDLER_FUNCTION():
+    #return OUTPUT OF YOUR SERVER CODE
+ 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
